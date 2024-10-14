40793700642800c967e0cc558df7dd17da2d2631
HunainAfzal-Dev <hunain.pc933@gmail.ccom>
2024-10-15 02:21:25 +0500

    Revert "first commit"
    This reverts commit 8373598f7855696df9f9f01da2296499317fb591.

--- src/app/(client)/home/page.tsx
+++ src/app/(client)/home/page.tsx
@@ -2,7 +2,7 @@
  
  function Home() {
    return (
-     <h1 className='text-white flex items-center justify-center h-full text-4xl '   >
+     <h1 className='text-white text-lg flex items-center justify-center h-full text-4xl '   >
        This is Home 
      </h1>
    )
